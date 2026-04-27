249ca599d13bd2a6b59e415e951a4b89998ba854
Narjisse <narjissebsaila@gmail.com>
2026-04-27 23:01:16 +0100

    Fix: remove Eclipse config files

--- src/controller/AdminRHController.java
+++ src/controller/AdminRHController.java
@@ -5,7 +5,6 @@ import dao.UtilisateurDAO;
 import model.Employe;
 import model.Utilisateur;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
